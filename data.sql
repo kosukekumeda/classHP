bea23c0cf435729bdb85391072125277071ad2a1
natpoppo <minaho0906@gmail.com>
2024-06-13 14:59:07 +0900

    sqlの変更　messageフォーム　recruitページ

--- data.sql
+++ data.sql
@@ -19,18 +19,35 @@ CREATE TABLE areas(
 );
 
 -- 3. areasテーブルにデータを追加
+
 INSERT INTO areas VALUES(null, '宮崎県', '宮崎市');
 INSERT INTO areas VALUES(null, '宮崎県', '都城市');
 INSERT INTO areas VALUES(null, '宮崎県', '延岡市');
-
--- 4. --2.officesテーブルを追加
-CREATE TABLE offices(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    area_id INT,
-    web VARCHAR(200),
-    -- foreign keyを設定
-    FOREIGN KEY (area_id) REFERENCES areas(id)
+INSERT INTO areas VALUES(null, '宮崎県', '日南市');
+INSERT INTO areas VALUES(null, '宮崎県', '小林市');
+INSERT INTO areas VALUES(null, '宮崎県', '日向市');
+INSERT INTO areas VALUES(null, '宮崎県', '串間市');
+INSERT INTO areas VALUES(null, '宮崎県', '西都市');
+INSERT INTO areas VALUES(null, '宮崎県', 'えびの市');
+INSERT INTO areas VALUES(null, '宮崎県', '三股町');
+INSERT INTO areas VALUES(null, '宮崎県', '高原町');
+INSERT INTO areas VALUES(null, '宮崎県', '国富町');
+INSERT INTO areas VALUES(null, '宮崎県', '綾町');
+INSERT INTO areas VALUES(null, '宮崎県', '高鍋町');
+INSERT INTO areas VALUES(null, '宮崎県', '新富町');
+INSERT INTO areas VALUES(null, '宮崎県', '西米良村');
+INSERT INTO areas VALUES(null, '宮崎県', '木城町');
+INSERT INTO areas VALUES(null, '宮崎県', '川南町');
+INSERT INTO areas VALUES(null, '宮崎県', '都農町');
+INSERT INTO areas VALUES(null, '宮崎県', '門川町');
+INSERT INTO areas VALUES(null, '宮崎県', '美郷町');
+INSERT INTO areas VALUES(null, '宮崎県', '高千穂町');
+INSERT INTO areas VALUES(null, '宮崎県', '日之影町');
+INSERT INTO areas VALUES(null, '宮崎県', '五ヶ瀬町');
+-- 4. --2.officesテーブルを追
+CREATE TABLE offices(    id INT AUTO_INCREMENT PRIARY KEY,
+    name VARCHAR(100) NOT NUL,
+    area_id INT,    web VARCHAR(200),    -- foreign keyを設定    FOREIGN KEY (area_id) REFRENCES areas(id)
 );
 
 -- 5. --3.jobsテーブルを追加
@@ -54,7 +71,7 @@ CREATE TABLE salaries(
 -- 8. --6.timesテーブルを追加
 CREATE TABLE times(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    time INT NOT NULL
+    time VARCHAR(20) NOT NULL
 );
 
 -- 9. --7.offsテーブルを追加
@@ -80,6 +97,7 @@ CREATE TABLE recruits(
     time_id INT,
     off_id INT,
     skills_id INT,
+    web VARCHAR(200),
     FOREIGN KEY (office_id) REFERENCES offices(id),
     FOREIGN KEY (job_id) REFERENCES jobs(id),
     FOREIGN KEY (worktype_id) REFERENCES worktypes(id),
@@ -97,33 +115,91 @@ CREATE TABLE recruits(
 -- 追加のサンプルデータを挿入
 
 -- offices テーブル
-INSERT INTO offices VALUES (NULL, '会社A', 1, 'http://example.com');
-INSERT INTO offices VALUES (NULL, '会社B', 2, 'http://example.com');
+INSERT INTO offices VALUES(NULL,'スパークジャパン',1,'http://www.sparkjapan.co.jp/');
+INSERT INTO offices VALUES(NULL,'UUUM',1,'https://www.uuum.co.jp/');
+INSERT INTO offices VALUES(NULL,'MANGO株式会社',1,'http://www.10005.co.jp/');
+INSERT INTO offices VALUES(NULL,'GMOインターネット',1,'https://www.gmo.jp/');
+INSERT INTO offices VALUES(NULL,'エイムネクスト',1,'https://www.aimnext.co.jp/');
+INSERT INTO offices VALUES(NULL,'オロ宮崎',1,'https://www.oro-miyazaki.com/');
+INSERT INTO offices VALUES(NULL,'アンティーシステム',1,'http://www.un-tsystem.com/');
+INSERT INTO offices VALUES(NULL,'グローバルテクノロジー',1,'https://gtmi.co.jp/');
+INSERT INTO offices VALUES(NULL,'パーソル ワークスデザイン',1,'https://www.persol-wd.co.jp/recruit/miyazaki/');
+INSERT INTO offices VALUES(NULL,'スカイコム',1,'https://www.skycom.jp/recruit/application/career/');
+INSERT INTO offices VALUES(NULL,'ソフトウェアセンター',1,'https://www.miyazaki-sc.co.jp/recruit/mid-career-recruit-entry/');
+INSERT INTO offices VALUES(NULL,'SCSKニアショアシステムズ',1,'https://www.scsk-nearshore.co.jp/about/');
+INSERT INTO offices VALUES(NULL,'ネオキャリア',1,'https://www.neocareer-miyazaki.com/');
+INSERT INTO offices VALUES(NULL,'スパークジャパン',1,'http://www.sparkjapan.co.jp/');
+INSERT INTO offices VALUES(NULL,'UUUM',1,'https://www.uuum.co.jp/');
 
 -- jobs テーブル
-INSERT INTO jobs VALUES (NULL, 'エンジニア');
-INSERT INTO jobs VALUES (NULL, 'デザイナー');
+INSERT INTO jobs VALUES(NULL,'開発エンジニア(SE)');
+INSERT INTO jobs VALUES(NULL,'アプリケーションエンジニア');
+INSERT INTO jobs VALUES(NULL,'プログラマー');
+INSERT INTO jobs VALUES(NULL,'組込みエンジニア');
+INSERT INTO jobs VALUES(NULL,'開発エンジニア(WEBエンジニア)');
+INSERT INTO jobs VALUES(NULL,'フロントエンドエンジニア');
+INSERT INTO jobs VALUES(NULL,'バックエンドエンジニア');
+INSERT INTO jobs VALUES(NULL,'WEBデザイナー');
+INSERT INTO jobs VALUES(NULL,'インフラエンジニア');
+INSERT INTO jobs VALUES(NULL,'サーバーエンジニア');
+INSERT INTO jobs VALUES(NULL,'ネットワークエンジニア');
+INSERT INTO jobs VALUES(NULL,'データベースエンジニア');
+INSERT INTO jobs VALUES(NULL,'セキュリティエンジニア');
+INSERT INTO jobs VALUES(NULL,'クラウドエンジニア');
+INSERT INTO jobs VALUES(NULL,'ITコンサルタント');
+INSERT INTO jobs VALUES(NULL,'プロジェクトマネージャー');
+INSERT INTO jobs VALUES(NULL,'社内SE');
+INSERT INTO jobs VALUES(NULL,'カスタマーサポート');
 
--- workTypes テーブル
-INSERT INTO workTypes VALUES (NULL, '正社員');
-INSERT INTO workTypes VALUES (NULL, 'パートタイム');
+-- workTypes テーブ
+INSERT INTO workTypes VALUES(NULL,'正社員');
+INSERT INTO workTypes VALUES(NULL,'パートタイム');
+INSERT INTO workTypes VALUES(NULL,'アルバイト');
+INSERT INTO workTypes VALUES(NULL,'契約社員');
+INSERT INTO workTypes VALUES(NULL,'派遣社員');
 
 -- salaries テーブル
-INSERT INTO salaries VALUES (NULL, '300万円');
-INSERT INTO salaries VALUES (NULL, '400万円');
+INSERT INTO salaries VALUES(NULL,'120,000~');
+INSERT INTO salaries VALUES(NULL,'130,000~');
+INSERT INTO salaries VALUES(NULL,'140,000~');
+INSERT INTO salaries VALUES(NULL,'150,000~');
+INSERT INTO salaries VALUES(NULL,'160,000~');
+INSERT INTO salaries VALUES(NULL,'170,000~');
+INSERT INTO salaries VALUES(NULL,'180,000~');
+INSERT INTO salaries VALUES(NULL,'190,000~');
+INSERT INTO salaries VALUES(NULL,'200,000~');
+INSERT INTO salaries VALUES(NULL,'210,000~');
+INSERT INTO salaries VALUES(NULL,'220,000~');
+INSERT INTO salaries VALUES(NULL,'230,000~');
+INSERT INTO salaries VALUES(NULL,'240,000~');
+INSERT INTO salaries VALUES(NULL,'250,000~');
+INSERT INTO salaries VALUES(NULL,'260,000~');
 
 -- times テーブル
-INSERT INTO times VALUES (NULL, 8);
-INSERT INTO times VALUES (NULL, 6);
+INSERT INTO times VALUES(NULL,'8:00-17:00');
+INSERT INTO times VALUES(NULL,'8:30-17:30');
+INSERT INTO times VALUES(NULL,'9:00-18:00');
+INSERT INTO times VALUES(NULL,'9:30-18:30');
+INSERT INTO times VALUES(NULL,'10:00-19:00');
+INSERT INTO times VALUES(NULL,'10:30-19:30');
+INSERT INTO times VALUES(NULL,'11:00-20:00');
+INSERT INTO times VALUES(NULL,'フレックスタイム');
 
 -- offs テーブル
-INSERT INTO offs VALUES (NULL, '土日休み');
-INSERT INTO offs VALUES (NULL, '週休2日');
+INSERT INTO offs VALUES(NULL,'土日休み');
+INSERT INTO offs VALUES(NULL,'週休2日制');
+INSERT INTO offs VALUES(NULL,'土日祝休み');
+INSERT INTO offs VALUES(NULL,'シフト制');
 
 -- skills テーブル
-INSERT INTO skills VALUES (NULL, 'Java');
-INSERT INTO skills VALUES (NULL, 'PHP');
+INSERT INTO skills VALUES(NULL,'Java');
+INSERT INTO skills VALUES(NULL,'PHP');
+INSERT INTO skills VALUES(NULL,'JavaScript');
+INSERT INTO skills VALUES(NULL,'HTML/CSS');
+INSERT INTO skills VALUES(NULL,'C#');
+INSERT INTO skills VALUES(NULL,'Python');
+INSERT INTO skills VALUES(NULL,'不問');
 
 -- recruits テーブル
-INSERT INTO recruits VALUES (NULL, CURRENT_TIMESTAMP, 1, 1, 1, 1, 1, 1, 1);
-INSERT INTO recruits VALUES (NULL, CURRENT_TIMESTAMP, 2, 2, 2, 2, 2, 2, 2);
+INSERT INTO recruits VALUES (NULL, CURRENT_TIMESTAMP, 11, 11, 2, 8, 3, 1, 1, 'https://www.inter-cross.com/job/show/3288');
+INSERT INTO recruits VALUES (NULL, CURRENT_TIMESTAMP, 2, 2, 2, 2, 2, 2, 2,'https://www.inter-cross.com/job/show/2117');
--- recruit.php
+++ recruit.php
@@ -12,15 +12,14 @@
     <!--======= ここからコピペ ==========-->
     <section id="reruit" class="wrapper">
     <div class="container">
-                    <h2 class="section-title"> - About - </h2>
+                    <h2 class="section-title"> - recruit - </h2>
                     <div class="description">
-                        <h3>人生のチャンスをつかもう。</h3>
-                        <p>手を伸ばせばすぐに届く、ITエンジニア科のあなたに一番近いwebサイトを作りました</p>
+                        <p>ITエンジニアの求人倍率はずばぬけて高いです。<br>今こそねらいめ！</p>
                     </div>
             </div>
     <div class="container2">
         <!-- ==============ここから==================== -->
-        <!-- <h2 class="section-title">Search Form</h1>
+        <h2 class="section-title">Search Form</h1>
         <div class="formbox">
             <form action="search.php" method="get">
                 <label for="job" class="label">職種:</label>
@@ -53,43 +52,7 @@
                 <button type="submit" class="submit">検索</button>
             </form>
         </div>
-        <div class="formbox"> -->
-            <!-- =================ここまで================ -->
-                    <h2 class="section-title">Message Form</h2>
-                    <div class="description">
-                        <h3>背中を押してあげよう</h3>
-                        <p>6ヶ月間ともに過ごした仲間へ、感謝の気持ちをこめて<br>メッセージや、気になった求人などなど..<br>匿名で誰かの元へ届けてみませんか？</p>
-                        <div class="messagebox">
-                            <p>投稿するメッセージを入力してください。</p>
-                            <form action="message.php" method="post">
-                            <label for="friend" class="label">送信先:</label>
-                                <select name="name" id="friend" class="select">
-                                    <option value="">選択してください</option>
-                                    <option value="1">加藤</option>
-                                    <option value="2">川添</option>
-                                    <option value="3">木村</option>
-                                    <option value="4">櫛間</option>
-                                    <option value="5">久米田</option>
-                                    <option value="6">小林</option>
-                                    <option value="7">友安</option>
-                                    <option value="8">橋本</option>
-                                    <option value="9">原田</option>
-                                </select>
-                            <label for="message" class="label">message</label>
-                            <input type="text" name="message" class="textbox">
-                            <input type="submit" value="送信" class="submit">
-                            </form>
-
-                        </div>
-                    </div>
-                    <div class="container2">
-                    <h2 class="section-title"> - - - - ✉Mail Box - - - - </h2>
-                    <div class="namebox">
-                    <a href="" class="btn btn-flat"><span>加藤</span></a>
-                    </div>
-                    </div>
-            </div>
-            </div>
+        <!-- ここまで -->
     </div>
     </section>
     <!-- =========== ここまでコピペ ========== -->
